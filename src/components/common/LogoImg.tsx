425aaacf79ffeb1227a8e66379d439e0aa500e99
Aswin K O <aswinko479@gmail.com>
2024-09-19 12:50:35 +0530

    minor changes meta data

--- src/components/common/LogoImg.tsx
+++ src/components/common/LogoImg.tsx
@@ -1,29 +1,28 @@
 "use client";
 
-import { useTheme } from 'next-themes';
-import { useEffect, useState } from 'react';
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export default function Logo() {
   const { theme, systemTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
-
   // console.log("ff", theme);
-  
+
   // This is necessary to avoid hydration mismatch
   useEffect(() => setMounted(true), []);
 
   if (!mounted) return null;
 
   // Check if the user prefers dark theme or light theme
-  const currentTheme = theme === 'system' ? systemTheme : theme;
+  const currentTheme = theme === "system" ? systemTheme : theme;
 
   return (
     <img
       src={
-        currentTheme === 'dark'
-          ? '/SAMA-white.png' // Dark mode logo
-          : '/SAMA-Final-02.png' // Light mode logo
+        currentTheme === "light"
+          ? "/SAMA-Final-02.png" // Light mode logo
+          : "/SAMA-white.png" // Dark mode logo
       }
       alt="logo"
       className="object-contain w-80 h-56"
